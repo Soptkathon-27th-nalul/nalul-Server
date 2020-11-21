c8b8346e9fd9bd7592d902f95fb97875e24c75e4
Suzie Lee <wnsuwls0707@gmail.com>
2020-11-22 07:58:41 +0900

    [ Add] Post edit

---
+++ routes/posts/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const postController = require('../../controller/postController');
+
+router.post('/', postController.createPost);
+
+router.get('/',postController.readPosts);
+
+
+
+module.exports = router;
